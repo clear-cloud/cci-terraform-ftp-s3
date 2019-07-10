b239be54ee4b19c7713075ef908ea0d83dddf3b1
Chris.Morgan <chris.morgan@clearcloud.co.uk>
2019-07-10 11:02:05 +0000

    init lc, asg and userdata

---
+++ lc.tf
@@ -0,0 +1,42 @@
+# ---------------------------------------
+# Launch Config
+# ---------------------------------------
+resource "aws_launch_configuration" "ftp" {
+  depends_on           = ["aws_eip.ftp"]
+  name_prefix          = "terraform-ftp-lc-"
+  image_id             = "${var.image_id}"
+  instance_type        = "${var.instance_type}"
+  key_name             = "${var.key_name}"
+  iam_instance_profile = "${aws_iam_instance_profile.ftp_instance_profile.name}"
+  security_groups      = ["${var.security_groups}"]
+  enable_monitoring    = "${var.enable_monitoring}"
+  user_data            = "${data.template_file.user_data.rendered}"
+
+  # Setup root block device
+  root_block_device {
+    volume_size = "${var.volume_size}"
+    volume_type = "${var.volume_type}"
+  }
+
+  # Create before destroy
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# ---------------------------------------
+# Render userdata bootstrap file
+# ---------------------------------------
+data "template_file" "user_data" {
+  depends_on = ["aws_eip.ftp"]
+  template   = "${file("${path.module}/userdata.sh")}"
+
+  vars {
+    hostname                    = "${var.hostname}"
+    dns_domain_name             = "${var.dns_domain_name}"
+    hosted_zone_id              = "${var.hosted_zone_id}"
+    supplementary_user_data_pre = "${var.supplementary_user_data_pre}"
+    supplementary_user_data     = "${var.supplementary_user_data}"
+  }
+}
+
---
+++ userdata.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# ----------------
+# Install custom additional set-up PRE
+# ----------------
+${supplementary_user_data_pre}
+# ----------------
+# Update host
+# ----------------
+yum update -y
+# ----------------
+# Install required utilities 
+# ----------------
+RequiredPackages=(awscli lsof git wget ruby)
+yum install -y "${RequiredPackages[@]}"
+# ----------------
+# Setup DNS record update
+# ----------------
+PUBLICIP=$(curl -s 'http://169.254.169.254/latest/meta-data/public-ipv4')
+cat << EOF > /root/dns_update.json
+{"Comment": "Bastion DNS record update","Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"${hostname}.${dns_domain_name}","Type":"A","TTL":300,"ResourceRecords":[{"Value":"$PUBLICIP"}]}}]}
+EOF
+# ----------------
+# Update DNS record
+# ----------------
+aws route53 change-resource-record-sets --hosted-zone-id ${hosted_zone_id} --change-batch file:///root/dns_update.json
+# ----------------
+# Set hostname
+# ----------------
+hostname ${hostname}.${dns_domain_name}
+hostnamectl set-hostname ${hostname}.${dns_domain_name}
+# ----------------
+# Setup SSM Agent
+# ----------------
+yum install amazon-ssm-agent python-deltarpm -y
+systemctl enable amazon-ssm-agent
+systemctl start amazon-ssm-agent
+echo "systemctl start amazon-ssm-agent" >> /etc/rc.local
+# ----------------
+# Install s3fs-fuse
+# ----------------
+yum install amazon-ssm-agent python-deltarpm gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap git automake fuse git openssl-devel jq -y
+git clone https://github.com/s3fs-fuse/s3fs-fuse --branch v1.85
+cd s3fs-fuse/
+./autogen.sh
+./configure --prefix=/usr --with-openssl
+make
+make install
+# ----------------
+# Install VSFTPD 
+# ----------------
+yum install vsftpd
+systemctl enable vsftpd && systemctl start vsftpd
+# ----------------
+# Install CodeDeploy agent
+# ----------------
+wget https://bucket-name.s3.region-identifier.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+# ----------------
+# Allow for additional commands
+# ----------------
+${supplementary_user_data}
+
